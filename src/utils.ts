788f7792f71100bccdea63672694e551037be7d1
Vaclav <mikeska@gmail.com>
2020-12-29 16:22:38 +0100

    fixed null check

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ import { exec } from "child_process";
 
 export class Utils {
     public static isNumberVersion(version: string) {
+        if (!version) {
+            return false;
+        }
+        
         let prms = version.split('.');
         let lastPrm = prms[prms.length - 1];
         let minorVersion = Number(lastPrm);
